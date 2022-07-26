63b2e071d8d4b6a35154efb073277d5f4442d8ed
xiaoguangli <guangmvp@gmail.com>
2022-07-26 18:56:27 +1000

    React components practice

---
+++ react-components/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import UnorderList from "./UnorderList";
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <UnorderList />
+  </div>,
+  document.getElementById("root")
+);
