658e894ec786aaa5cead6b4a3165002bd1c40ab0
xiaoguangli <guangmvp@gmail.com>
2022-07-29 09:50:04 +1000

    First version of Keeper

---
+++ my-app/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+function Footer(){
+
+    const year = new Date().getFullYear();
+    return <p className="footer">Copyright {year}</p>;
+}
+
+export default Footer;
