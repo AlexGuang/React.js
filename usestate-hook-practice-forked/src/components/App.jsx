67980fe0d4fbc5ad939a45b4236feec7e3d9d27d
xiaoguangli <guangmvp@gmail.com>
2022-08-05 14:24:08 +1000

    Complete the state practice

--- usestate-hook-practice-forked/src/components/App.jsx
+++ usestate-hook-practice-forked/src/components/App.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React ,{useState} from "react";
 
 function App() {
+  const [newTime,setTime] = useState(new Date().toLocaleTimeString());
+  //let time = new Date().toLocaleTimeString();
+  function getTime(){
+    setTime(new Date().toLocaleTimeString())
+  }
+  const [newTimes,setTimes] = useState(new Date().toLocaleTimeString());
+  function sayHi() {
+    setTimes(new Date().toLocaleTimeString())
+  }
+  {setInterval(sayHi, 1000)}
+//console.log(time);
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{newTime}</h1>
+     
+
+      <button onClick={getTime}>Get Time</button>
+      <h1>{newTimes}</h1>
+     
     </div>
   );
 }
