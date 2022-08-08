06b1aeb311e5012e0215ed9c0485ff240fa0474d
xiaoguangli <guangmvp@gmail.com>
2022-08-08 14:28:05 +1000

    Add form and preventDefault method

--- event-handling-in-react-forked/src/components/App.jsx
+++ event-handling-in-react-forked/src/components/App.jsx
@@ -9,26 +9,30 @@ function App() {
 const [headerText,setHeaderText] = useState("");
 const [isMouseOver,setIsmouseover] = useState(false);
 const [name,setName] = useState("");
-function submitButton(){
+function submitButton(event){
   
   setHeaderText(name);
+  event.preventDefault();
   
 }
   return (
     <div className="container">
       <h1>Hello {headerText}</h1>
-      <input 
-      type="text" 
-      placeholder="What's your name?"
-      value = {name}
-      onChange={(event)=>{setName(event.target.value);
-      }} />
-      <button 
-      onClick = {submitButton} 
-       onMouseOver ={()=>setIsmouseover(current=>!current)} 
-       onMouseOut = {()=>setIsmouseover(current =>!current)}
-       style ={{backgroundColor:isMouseOver? "black":"white"}}
-       >Submit</button>
+      <form onSubmit={submitButton}>
+        <input 
+        type="text" 
+        placeholder="What's your name?"
+        value = {name}
+        onChange={(event)=>{setName(event.target.value);
+        }} />
+        <button 
+        type = "submit"
+        onMouseOver ={()=>setIsmouseover(current=>!current)} 
+        onMouseOut = {()=>setIsmouseover(current =>!current)}
+        style ={{backgroundColor:isMouseOver? "black":"white"}}
+        >Submit</button>
+      </form>
+      
     </div>
   );
 }
