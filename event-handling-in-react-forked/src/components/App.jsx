69700b5a1da8cbe87d809862c628745a533cd96d
xiaoguangli <guangmvp@gmail.com>
2022-08-08 12:55:07 +1000

    Complete the mouseOver handler with state of react

--- event-handling-in-react-forked/src/components/App.jsx
+++ event-handling-in-react-forked/src/components/App.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
+
+
+
+
 
 function App() {
+
+const [headerText,setHeaderText] = useState("Hello");
+const [isMouseOver,setIsmouseover] = useState(false);
+function submitButton(){
+  setHeaderText("Submited");
+}
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headerText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button 
+      onClick = {submitButton} 
+       onMouseOver ={()=>setIsmouseover(true)} 
+       onMouseOut = {()=>setIsmouseover(false)}
+       style ={{backgroundColor:isMouseOver? "black":"white"}}
+       >Submit</button>
     </div>
   );
 }
