1d7c709daf1bd23a408ccde1baea40bed8311fd3
xiaoguangli <guangmvp@gmail.com>
2022-08-13 20:38:10 +1000

    Undebug project

--- Keeper App Project Part 3/src/components/App.jsx
+++ Keeper App Project Part 3/src/components/App.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React,{ useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+const[items ,setItems] = useState([{
+  title:"Note title",
+  content:"Note content"
+}]);
+function onAdd(item){
+setItems((prep)=>[...prep,item]);
+console.log("items:"+items)
+
+}
+function deleteItem(id){
+  setItems(
+    (prep)=>{
+      prep.filter(
+        (item,index)=>index !== id)
+    }
+  )
+}
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea 
+        onAdd={onAdd}
+      />
       <Note key={1} title="Note title" content="Note content" />
+      {items.map((item,index)=>{
+     return   <Note key={index}  id = {index} title={item.title} content={item.content}
+     onDelete={deleteItem} />
+      })}
+      
       <Footer />
     </div>
   );
--- Keeper App Project Part 3/src/components/CreateArea.jsx
+++ Keeper App Project Part 3/src/components/CreateArea.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React ,{useState} from "react";
 
-function CreateArea() {
+
+function CreateArea(props) {
+  const[textInput, setTextinput] = useState({
+    title:"",
+    content:""
+  });
+
+  function onInput(event){
+    let newData ;
+    if(event.target.name ==="title"){
+      setTextinput((prep)=>{
+     
+     newData = prep;
+     newData.title = event.target.value
+    return newData})}
+    else if(event.target.name==="content"){
+      setTextinput((prep)=>{
+     
+        newData = prep;
+        newData.content = event.target.value
+       return newData})}
+       console.log(textInput)
+    }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+        onChange ={(event) =>onInput(event)}
+         name="title" placeholder="Title"  value = {textInput.title} />
+        <textarea 
+        onChange = {(event) =>onInput(event)}
+        name="content" placeholder="Take a note..." rows="3" value = {textInput.content}  />
+        <button onClick = {(event)=>{
+        props.onAdd(textInput);
+        event.preventDefault();
+        textInput = {
+          title:"",
+          content:""
+        }}
+        }>Add</button>
       </form>
     </div>
   );
