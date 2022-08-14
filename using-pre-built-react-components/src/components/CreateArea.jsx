5177ddc42cb74a0dd506952a3a7b59a23800560c
xiaoguangli <guangmvp@gmail.com>
2022-08-14 12:57:35 +1000

    Use material ui pre component to fuctionalize the keeper app

--- using-pre-built-react-components/src/components/CreateArea.jsx
+++ using-pre-built-react-components/src/components/CreateArea.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+  const[isActivated, setIsactivated] = useState(false)
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -26,7 +29,13 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
-  return (
+function onExpand(){
+  setIsactivated(true);
+}
+
+
+
+  return(
     <div>
       <form className="create-note">
         <input
@@ -34,18 +43,27 @@ function CreateArea(props) {
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
+          style={{display : isActivated? "block":"none"}}
         />
+     
         <textarea
           name="content"
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          onClick ={onExpand}
+          rows={isActivated?"3":"1"}
         />
-        <button onClick={submitNote}>Add</button>
-      </form>
+        <Zoom in={isActivated?true:false}> 
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom> 
+        </form>
     </div>
-  );
+  )
+
+ 
 }
 
 export default CreateArea;
